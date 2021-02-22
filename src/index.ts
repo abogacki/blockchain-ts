fb2ab8dd59d56242cbd990f0bca1384beef27250
abogacki <anbogacki@gmail.com>
2021-02-22 23:36:44 +0100

    Add entry

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import moment from "moment";
+import Blockchain from "src/blockchain";
+import Block from "./block";
+
+const data = [
+  {
+    timestamp: moment().add("2", "days").toDate().getTime(),
+    data: { transaction: "some-transaction", amount: 5 },
+  },
+  {
+    timestamp: moment().add("3", "days").toDate().getTime(),
+    data: { transaction: "next-transaction", amount: 30 },
+  },
+  {
+    timestamp: moment().add("4", "days").toDate().getTime(),
+    data: { transaction: "third-transaction", amount: 12.5 },
+  },
+];
+
+const blockchain = new Blockchain();
+
+data.forEach(({ timestamp, data }) =>
+  blockchain.addBlock(new Block(timestamp, data))
+);
+
+console.log(JSON.stringify({ blockchain }, null, 4));
+
+console.log("Blockchain valid? " + blockchain.isChainValid());
